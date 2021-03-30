89af8dff66262d1506a7b1f3303ada79ef362fbc
d-krolicki <80037191+d-krolicki@users.noreply.github.com>
2021-03-30 12:53:05 +0200

    Create matrixLib.py

---
+++ pythonVersion/matrixLib.py
@@ -0,0 +1,21 @@
+import numpy as nmp
+
+
+def matrixCreate(row, col):
+
+    matrix = []
+
+    print("Enter your numbers in a single line, each separated by a spacebar:\n")
+
+    numbers = list(map(int, input().split()))
+    matrix = nmp.array(numbers).reshape(row, col)
+    return matrix
+
+def matrixAdd(matrix1, matrix2):
+    return nmp.add(matrix1, matrix2)
+
+def matrixSubt(matrix1, matrix2):
+    return nmp.subtract(matrix1, matrix2)
+
+def matrixMul(matrix1, matrix2):
+    return nmp.multiply(matrix1, matrix2)
